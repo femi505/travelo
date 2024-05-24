0c68a00577efd7d0b8923ecdbdceb48e3daaacee
Femi Lawal <femilawal505@gmail.com>
2024-05-24 22:44:15 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from "react";
+import Background from "./Components/Background/Background";
+import Navbar from "./Components/Navbar/Navbar";
+import Hero from "./Components/Hero/Hero";
+
+const App = () => {
+  let heroData = [
+    { text1: "Dive into", text2: "what you love" },
+    { text1: "Indulge", text2: "your passions" },
+    { text1: "Follow", text2: "your fantasy" },
+  ];
+
+  const [heroCount, setHeroCount] = useState(0);
+  const [playStatus, setPlayStatus] = useState(false);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setHeroCount((count) => (count === 2 ? 0 : count + 1));
+    }, 3000);
+  
+    return () => clearInterval(intervalId);
+  }, []);
+  
+  
+
+  return (
+    <div>
+      <Background playStatus={playStatus} heroCount={heroCount} />
+      <Navbar/>
+      <Hero
+          setPlayStatus={setPlayStatus}
+          heroData={heroData[heroCount]}
+          heroCount={heroCount}
+          setHeroCount={setHeroCount}
+          playStatus={playStatus}
+          />
+    </div>
+  );
+};
+
+export default App;
